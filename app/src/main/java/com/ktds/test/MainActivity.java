762457dccf567efc5562645f994ecda105b83339
leesangyoon <dltkddbsgkgk@naver.com>
2015-07-28 13:29:24 +0900

    commit2

--- app/src/main/java/com/ktds/test/MainActivity.java
+++ app/src/main/java/com/ktds/test/MainActivity.java
@@ -1,15 +1,13 @@
 package com.ktds.test;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 
 public class MainActivity extends ActionBarActivity {
 
     //Hello////
-
+    //Hello2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
