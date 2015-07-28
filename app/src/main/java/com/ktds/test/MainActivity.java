b2451c1f2012d25d64ce2bdfba675a5453814023
leesangyoon <dltkddbsgkgk@naver.com>
2015-07-28 13:24:09 +0900

    commit

--- app/src/main/java/com/ktds/test/MainActivity.java
+++ app/src/main/java/com/ktds/test/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    //Hello////
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/ktds/test/helo/Test.java
@@ -0,0 +1,7 @@
+package com.ktds.test.helo;
+
+/**
+ * Created by 206-027 on 2015-07-28.
+ */
+public class Test {
+}
